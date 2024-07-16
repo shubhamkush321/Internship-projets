41eb7c375041f3ecfd5e3eca9d0a20d681bf9fa9
shubham <142429585+shubhamkush321@users.noreply.github.com>
2024-07-16 20:17:09 +0530

    Ineubytes

--- Major task Ecom/frontend/src/components/BannerProduct.js
+++ Major task Ecom/frontend/src/components/BannerProduct.js
@@ -1,16 +1,19 @@
 import React, { useEffect, useState } from 'react'
-import image1 from '../assest/banner/img1.webp'
-import image2 from '../assest/banner/img2.webp'
+import image1 from '../assest/banner/img1.png'
+import image2 from '../assest/banner/img2.png'
 import image3 from '../assest/banner/img3.jpg'
-import image4 from '../assest/banner/img4.jpg'
+import image4 from '../assest/banner/img4.png'
 import image5 from '../assest/banner/img5.webp'
+import image6 from '../assest/banner/img6.png'
 
 
-import image1Mobile from '../assest/banner/img1_mobile.jpg'
-import image2Mobile from '../assest/banner/img2_mobile.webp'
+import image1Mobile from '../assest/banner/img1.png'
+import image2Mobile from '../assest/banner/img2.png'
 import image3Mobile from '../assest/banner/img3_mobile.jpg'
-import image4Mobile from '../assest/banner/img4_mobile.jpg'
+import image4Mobile from '../assest/banner/img4.png'
 import image5Mobile from '../assest/banner/img5_mobile.png'
+import image5Mobile from '../assest/banner/img6.png'
+
 
 import { FaAngleRight } from "react-icons/fa6";
 import { FaAngleLeft } from "react-icons/fa6";
@@ -24,7 +27,8 @@ const BannerProduct = () => {
         image2,
         image3,
         image4,
-        image5
+        image5,
+        image6
     ]
 
     const mobileImages = [
@@ -32,7 +36,8 @@ const BannerProduct = () => {
         image2Mobile,
         image3Mobile,
         image4Mobile,
-        image5Mobile
+        image5Mobile,
+        image1Mobile
     ]
 
     const nextImage = () =>{
